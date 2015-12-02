3f71b83710dca88565415fa06af60d3b5ad593dc
unknown <tom.fougere@gmail.com>
2015-12-02 16:53:24 +0100

    start node in package.json

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-//var configDB = require('./config/database.js');
+var configDB = require('./config/database.js');
 
 // configuration ===============================================================
 //mongoose.connect(configDB.url); // connect to our database
