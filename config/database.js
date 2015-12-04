35ad9e0653df3a635e4973a628c891419889205b
Thomas F <tom.fougere@gmail.com>
2015-12-04 17:21:53 +0100

    Update database.js
    delete connexion uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
     'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    //mongodb://<dbuser>:<dbpassword>@ds061954.mongolab.com:61954/heroku_0srnc6ms
-    //mongodb://heroku_0srnc6ms:oleqvi7sj9k5euq3ims3ec3pal@ds061954.mongolab.com:61954/heroku_0srnc6ms
-    //{ user: "heroku_0srnc6ms", account: "heroku_0srnc6ms" }
+    //no need, we use heroku var env
 };
