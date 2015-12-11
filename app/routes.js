930c362a415a52349cb683d77eeaea85109ab4da
unknown <tom.fougere@gmail.com>
2015-12-11 15:21:50 +0100

    login with facebook

--- app/routes.js
+++ app/routes.js
@@ -52,6 +52,19 @@ module.exports = function(app, passport) {
     });
 
     // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    // =====================================
     // LOGOUT ==============================
     // =====================================
     app.get('/logout', function(req, res) {
@@ -63,8 +76,9 @@ module.exports = function(app, passport) {
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
     // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
+    if (req.isAuthenticated()){
         return next();
+    }
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,12 @@
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 // load up the user model
 var User            = require('../app/models/user');
+
+// load the auth variables
+var configAuth = require('./auth');
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
@@ -45,8 +49,9 @@ module.exports = function(passport) {
                 // we are checking to see if the user trying to login already exists
                 User.findOne({ 'local.email' :  email }, function(err, user) {
                     // if there are any errors, return the error
-                    if (err)
+                    if (err) {
                         return done(err);
+                    }
 
                     // check to see if theres already a user with that email
                     if (user) {
@@ -88,19 +93,74 @@ module.exports = function(passport) {
             // we are checking to see if the user trying to login already exists
             User.findOne({ 'local.email' :  email }, function(err, user) {
                 // if there are any errors, return the error before anything else
-                if (err)
+                if (err) {
                     return done(err);
+                }
 
                 // if no user is found, return the message
-                if (!user)
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                if (!user) {
+                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+                    // req.flash is the way to set flashdata using connect-flash
+                }
 
                 // if the user is found but the password is wrong
-                if (!user.validPassword(password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                if (!user.validPassword(password)){
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+                    // create the loginMessage and save it to session as flashdata
+                }
 
                 // all is well, return successful user
                 return done(null, user);
+            });
+
+        }));
+
+
+    // =========================================================================
+    // FACEBOOK ================================================================
+    // =========================================================================
+    passport.use(new FacebookStrategy({
+            // pull in our app id and secret from our auth.js file
+            clientID        : configAuth.facebookAuth.clientID,
+            clientSecret    : configAuth.facebookAuth.clientSecret,
+            callbackURL     : configAuth.facebookAuth.callbackURL,
+            profileFields   : ["emails", "displayName"]
+        },
+        // facebook will send back the token and profile
+        function(token, refreshToken, profile, done) {
+            // asynchronous
+            process.nextTick(function() {
+                // find the user in the database based on their facebook id
+                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                    // if there is an error, stop everything and return that
+                    // ie an error connecting to the database
+                    if (err) {
+                        return done(err);
+                    }
+                    // if the user is found, then log them in
+                    if (user) {
+                        return done(null, user); // user found, return that user
+                    } else {
+                        // if there is no user found with that facebook id, create them
+                        var newUser            = new User();
+
+                        // set all of the facebook information in our user model
+                        newUser.facebook.id    = profile.id; // set the users facebook id
+                        newUser.facebook.token = token; // we will save the token that facebook provides to the user
+                        newUser.facebook.name = profile.displayName; // look at the passport user profile to see how names are returned
+                        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+
+                        // save our user to the database
+                        newUser.save(function(err) {
+                            if (err) {
+                                throw err;
+                            }
+                            // if successful, return the new user
+                            return done(null, newUser);
+                        });
+                    }
+
+                });
             });
 
         }));
